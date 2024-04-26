9d96b83a165666db9eae683140941efb0799ded0
aduahimma23 <aduahimma18@gmail.com>
2024-04-26 22:24:43 +0530

    Update

--- main/models.py
+++ main/models.py
@@ -130,7 +130,7 @@ class Department(models.Model):
         return self.name
 
 
-class Courses(models.Model):
+class Section(models.Model):
     department = models.ForeignKey(Department, on_delete=models.CASCADE)
     name_of_course = models.CharField(max_length=150, unique=True, blank=False)
     videofile = models.FileField(upload_to="tech_hive_videos", unique=True)
@@ -163,7 +163,52 @@ class MultimediaHive(models.Model):
     name = models.CharField(max_length=255, unique=True, blank=False)
     videofile = models.FileField(upload_to='farm_hive', unique=True, blank=False)
     description = models.CharField(max_length=255, blank=False)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+class TechHive(models.Model):
+    departments = models.ForeignKey(Department, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, unique=True, blank=False)
+    videofile = models.FileField(upload_to='farm_hive', unique=True, blank=False)
+    description = models.CharField(max_length=255, blank=False)
     _date_created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self) -> str:
         return self.name
+    
+class Prices(models.Model):
+    tech_hive_price = models.ForeignKey(TechHive, on_delete=models.CASCADE)
+    farm_hive_price = models.ForeignKey(FarmHive, on_delete=models.CASCADE)
+    multimedia_hive_price = models.ForeignKey(MultimediaHive, on_delete=models.CASCADE)
+    amount = models.DecimalField(decimal_places=2, max_digits=6, blank=False)
+    currency = models.CharField(max_length=3)
+    date_created = models.DateField(auto_now_add=True)
+
+class Promotions(models.Model):
+    name = models.CharField(max_length=255)
+    is_promotion_active = models.BooleanField(default=False)
+    get_amount = models.ForeignKey(Prices, on_delete=models.CASCADE)
+    discount_percentage = models.DecimalField(max_digits=5, decimal_places=2)
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+    def __str__(self):
+        return self.name
+    
+    def calculate_discounted_amount(self):
+        if self.is_promotion_active:
+            discount_amount = self.get_amount.amount - (self.get_amount.amount * self.discount_percentage / 100)
+            return discount_amount
+        else:
+            return self.get_amount
+
+class Locations(models.Model):
+    country = models.CharField(max_length=255, unique=True, blank=False)
+    country_flag = models.ImageField(upload_to='country_flags')
+    city = models.CharField(max_length=255, unique=True, blank=False)
+    date_created = models.DateField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.country, self.city, self.date_created
